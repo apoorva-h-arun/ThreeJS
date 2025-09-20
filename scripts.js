978ce26e372fdb38b3f7d480832964abef552a07
apoorva-h-arun <apoorvaharun.cs24@rvce.edu.in>
2025-09-20 13:04:44 +0530

    Pictures

--- scripts.js
+++ scripts.js
@@ -1,10 +1,6 @@
 import * as THREE from 'three';
 import {OrbitControls, ThreeMFLoader} from 'three/examples/jsm/Addons.js';
 
-const grassImg = document.createElement('img');
-grassImg.src = 'https://9bisfjtoi2cc6ko3.public.blob.vercel-storage.com/GolfGrass.jpg';
-//document.body.appendChild(grassImg);
-
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement)
@@ -26,16 +22,16 @@ scene.add(ambientLight);
 const directionalLight = new THREE.DirectionalLight(0xFFFFFF, 1.5);
 scene.add(directionalLight);
 
-const grassTexture = new THREE.TextureLoader().load(grassImg);
-const woodTexture = new THREE.TextureLoader().load('./assets/Wood.avif');
+const grassTexture = new THREE.TextureLoader().load('https://9bisfjtoi2cc6ko3.public.blob.vercel-storage.com/GolfGrass.jpg');
+const woodTexture = new THREE.TextureLoader().load('https://9bisfjtoi2cc6ko3.public.blob.vercel-storage.com/Wood.avif');
 const cubeTextureLoader = new THREE.CubeTextureLoader();
 scene.background = cubeTextureLoader.load([
-  './assets/PixelSky.png',
-  './assets/PixelSky.png',
-  './assets/PixelSky.png',
-  './assets/PixelSky.png',
-  './assets/PixelSky.png',
-  './assets/PixelSky.png'
+  'https://9bisfjtoi2cc6ko3.public.blob.vercel-storage.com/PixelSky.png',
+  'https://9bisfjtoi2cc6ko3.public.blob.vercel-storage.com/PixelSky.png',
+  'https://9bisfjtoi2cc6ko3.public.blob.vercel-storage.com/PixelSky.png',
+  'https://9bisfjtoi2cc6ko3.public.blob.vercel-storage.com/PixelSky.png',
+  'https://9bisfjtoi2cc6ko3.public.blob.vercel-storage.com/PixelSky.png',
+  'https://9bisfjtoi2cc6ko3.public.blob.vercel-storage.com/PixelSky.png'
 ]);
 
 const ballGeometry = new THREE.SphereGeometry(0.5);
